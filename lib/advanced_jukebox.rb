5ae94177960f891cbb5c028f40a12c36453af870
Joan in DC <joanrigdon@gmail.com>
2018-12-29 16:24:55 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -1,6 +1,6 @@
 #require_pry
 
-#Here is the song hash you will be working with. Each key is a song name and each value is the location of it's mp3 file.
+#Here is the song hash you will be working with. Each key is a song name and each value is the location of its mp3 file.
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 
